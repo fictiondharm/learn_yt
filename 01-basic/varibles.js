4a0bbcce08acd140f21165655597d5fb627918c3
fictiondharm <151057980+fictiondharm@users.noreply.github.com>
2025-04-05 17:52:32 +0000

    we have leant about the varbles and constants

---
+++ 01-basic/varibles.js
@@ -0,0 +1,22 @@
+const accoutId =144553
+let accountEmail = "dharme@gmail.com"
+var accountpassword ="122"
+accountCity  ="jaipur"
+let accontState;
+// so if dont add/assign any values to varibale it prints as the undfined...
+accountId = 34 // not allowed first printed
+
+
+accountEmail="rinkya@gmail.com"
+
+
+accountpassword = "19887"
+
+/* 
+we dont use the var nowdays, as it was making an issues in the block scopes. so we generally got with the let
+*/
+
+
+accountCity = "karntka"
+
+console.table([accountEmail, accountpassword, accoutId , accountCity, accontState])
