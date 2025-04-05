01d69711b33e586c00038943695fcf432f3d6ff0
fictiondharm <151057980+fictiondharm@users.noreply.github.com>
2025-04-05 18:33:49 +0000

    datatypes learn

---
+++ 01-basic/02-Datatypes.js
@@ -0,0 +1,31 @@
+"use strict" // we bascially this for the upated versions of the js.
+
+console.log(3 
+    +3// avoid in this way of writing the code, it is very bad , make code redable
+); 
+
+console.log("dharme bro");
+
+/* we use mdn and tc39.es for the documenttation
+documnetations are the one from where we can get the 
+proper info like these are the startndards which 
+says how something should be..*/
+
+
+let name ="dharme"  //  string datatype
+let  age = 18     // number
+let isLoggedIn = false
+
+// number => 2 to power 53 is range
+//  bigint used fopr larger one like trading paltofrms.. 
+//  string which are given ""
+//  boolean => true/flase
+// null (standalone value) kali honaa represnt kartaa hai..
+// undefined => when value is not assignned
+//  typeof shows which type of datatype it is
+// symbols are used to define uniquness
+
+console.log(typeof null);
+
+// null is object 
+// undefined is a undefined a special datatype..
